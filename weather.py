2e8213229932ac93d2186510047ededde29c0a6e
Merhii <themerhi@hotmail.com>
2025-03-31 00:16:50 +0300

    automation of weather

---
+++ weather.py
@@ -0,0 +1,30 @@
+import requests
+import pandas as pd 
+
+import apiKey as key
+API_KEY = key.API_KEY
+
+LOCATION = "Beirut"
+# URL = f"https://api.weatherapi.com/v1/forecast.json?key={API_KEY}&q={LOCATION}&day=10"
+# URL = f"https://api.weatherapi.com/v1/future.json?key={API_KEY}&q={LOCATION}&dt=2025-04-"
+
+locations = ["Beirut", "Paris", "Riyad", "Cairo","LA"]
+arr = []
+for LOCATION in locations:
+    URL = f"https://api.weatherapi.com/v1/forecast.json?key={API_KEY}&q={LOCATION}&days=3"
+    response = requests.get(URL)
+    data = response.json()
+ 
+    for forecast_day in data['forecast']['forecastday']:
+        date = forecast_day['date']
+        average_temp = forecast_day['day']['avgtemp_c']
+
+        arr.append({
+            "Date": date,
+            "Average Temp": average_temp,
+            "Location": LOCATION
+        })
+# Print the array or convert it to DataFrame
+df = pd.DataFrame(arr)
+print(df)
+df.to_csv("weather.csv", mode='a', index=False, header=False)
